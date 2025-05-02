36744763fa2f8e58069b72f5c2bc9e98d0ed8ae1
Mr.Dangle <maheshd1509@gmail.com>
2025-05-02 19:55:01 +0530

    *update 8

--- app/src/main/java/com/example/lbgassesment/presentation/state/ProductListState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.lbgassesment.presentation.state
-
-import com.example.lbgassesment.domain.model.ProductItem
-
-data class ProductListState(
-    val isLoading: Boolean = false,
-    val data: List<ProductItem>? = null,
-    var error: String = ""
-)
--- app/src/main/java/com/example/lbgassesment/utils/Constant.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lbgassesment.utils
-
-object Constant {
-    const val BASE_URL = "https://fakestoreapi.com/"
-}
